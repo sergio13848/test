e5345215ba999ec9cb5b646964f2ab16d0f30a4d
sergio <sergio13848@gmail.com>
2013-09-02 00:54:18 +0800

    modi

---
+++ src/com/sergio/gitTest/Test.java
@@ -0,0 +1,17 @@
+package com.sergio.gitTest;
+/**
+ * @ClassName: Test
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Sergio Han
+ * @date 2013-9-2 上午12:52:51
+ *
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("sfff");
+	}
+}
+
